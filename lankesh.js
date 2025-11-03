813a446ba32b419f845e0a5385eaf75d06fe6b16
lankeshyadav <lankeshk695@gmail.com>
2025-11-03 22:25:49 +0530

    commit

---
+++ lankesh.js
@@ -0,0 +1,18 @@
+// const b = "Lankesh";
+// console.log(b);
+
+// let a = 20;
+// console.log(a);
+
+
+// let a = "kumar";
+// console.log(a);
+
+
+// let num = 6;
+// console.log(sumtwo(num));
+
+const a = 5;
+const b = 5;
+const sum = a + b;
+console.log(sum);
